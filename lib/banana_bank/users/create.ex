a2462619ec213802916cf01f463bed97a167066c
costadv <dvcosta0505@outlook.com>
2026-09-03 19:46:08 -0300

    Adjustments to users/create.ex handle function accordingly to class.

--- lib/banana_bank/users/create.ex
+++ lib/banana_bank/users/create.ex
@@ -6,10 +6,10 @@ defmodule BananaBank.Users.Create do
     params
     |> User.changeset()
     |> Repo.insert()
-    |> handle_insert()
+    #|> handle_insert()
   end
 
-  defp handle_insert({:ok, user}), do: user
-  defp handle_insert({:error, changeset}), do: changeset
+  #defp handle_insert({:ok, user}), do: user
+  #defp handle_insert({:error, changeset}), do: changeset
 
 end
