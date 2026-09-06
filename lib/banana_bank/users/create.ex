a810776ac7145d8ede204fdbea0dac2b6a2a9ad0
costadv <dvcosta0505@outlook.com>
2026-09-06 20:07:29 -0300

    Added cep validation to user create function.

--- lib/banana_bank/users/create.ex
+++ lib/banana_bank/users/create.ex
@@ -1,12 +1,15 @@
 defmodule BananaBank.Users.Create do
   alias BananaBank.Users.User
   alias BananaBank.Repo
+  alias BananaBank.ViaCep.Client, as: ViaCepClient
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
-    #|> handle_insert()
+  def call(%{"cep" => cep} = params) do
+    with {:ok, _body} <- ViaCepClient.call(cep) do
+      params
+      |> User.changeset()
+      |> Repo.insert()
+      #|> handle_insert()
+    end
   end
 
   #defp handle_insert({:ok, user}), do: user
