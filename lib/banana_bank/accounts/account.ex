c80fa9b41ab868b34fdbfdfe2e0dbf4ea280c8e5
costadv <dvcosta0505@outlook.com>
2026-09-07 18:31:09 -0300

    Created migration and schema for accounts.

---
+++ lib/banana_bank/accounts/account.ex
@@ -0,0 +1,27 @@
+defmodule BananaBank.Accounts.Account do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias BananaBank.Users.User
+
+  @required_params [:balance, :user_id]
+  schema "accounts" do
+    field :balance, :decimal
+    belongs_to :user, User
+
+      timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> check_constraint(:balance, name: :balance_must_be_positive)
+  end
+
+  def changeset(%__MODULE__{} = account, params) do
+    account
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> check_constraint(:balance, name: :balance_must_be_positive)
+  end
+end
