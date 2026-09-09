0065b0366d4960d36759129acce2a2c38f06ade1
costadv <dvcosta0505@outlook.com>
2026-09-09 20:44:28 -0300

    Created token.ex to handle web tokens.

---
+++ lib/banana_bank_web/token.ex
@@ -0,0 +1,20 @@
+defmodule BananaBankWeb.Token do
+  alias Phoenix.Token
+  alias BananaBankWeb.Endpoint
+
+  @sign_salt "banana_bank_api"
+
+  def sign(user) do
+    Token.sign(Endpoint, @sign_salt, %{user_id: user.id})
+  end
+
+  # if needed, can be used like this to handle other cases.
+  # def verify(token) do
+    # case Token.verify(Endpoint, @sign_salt, token) do
+    #   {:ok, _result} = result -> result
+    #   {:error, _} = error -> error
+  #   end
+  # end
+
+  def verify(token), do: Token.verify(Endpoint, @sign_salt, token)
+end
