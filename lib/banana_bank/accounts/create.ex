0fa1d090bf5fed962ea2e1b0f398aa0ba1768a8b
costadv <dvcosta0505@outlook.com>
2026-09-09 16:41:05 -0300

    Added check to verify if user exists before creating account.

--- lib/banana_bank/accounts/create.ex
+++ lib/banana_bank/accounts/create.ex
@@ -1,11 +1,18 @@
 defmodule BananaBank.Accounts.Create do
   alias BananaBank.Accounts.Account
   alias BananaBank.Repo
+  alias BananaBank.Users.Get, as: UserGet
 
-  def call(params) do
+  def call(%{"user_id" => id} = params) do
+    case UserGet.call(id) do
+      {:ok, _} -> create_account(params)
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp create_account(params) do
     params
     |> Account.changeset()
     |> Repo.insert()
   end
-
 end
